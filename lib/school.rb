df6fce70d96810bca7f882446384e96bc61b05dc
Enoch2k2 <enoch2k2@hotmail.com>
2019-06-19 10:40:47 -0500

    created school class, added instance methods to school class

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+require 'pry'
+
+class School 
+  attr_reader :roster
+
+  def initialize(school_name)
+    @school_name = school_name 
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    roster[grade] ||= []
+    roster[grade] << student
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+  # def roster 
+  #   @roster 
+  # end
+
+  def sort
+    roster.each do |grade, students|
+      students.sort!
+    end
+  end
+end
